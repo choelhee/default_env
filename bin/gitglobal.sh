54efe070841ed18429c45e8a3dfb32bf8eaf5fec
choelhee <paekun@gmail.com>
2018-09-10 16:37:26 +0900

    add gitglobal.sh

---
+++ bin/gitglobal.sh
@@ -0,0 +1,14 @@
+
+
+# gitlab
+# user.name "choelhee.lee"
+# user.email "paekun@gmail.com"
+# github (private)
+# user.name "choelhee"
+# user.email "paekun@gmail.com"
+# github (public)
+# user.name "choelheelee"
+# user.email "chlee@sunjesoft.com"
+
+git config --global user.name "$1"
+git config --global user.email "$2"
