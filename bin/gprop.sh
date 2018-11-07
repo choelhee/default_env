c4184d07ad49b2d670787c89054df5852d532d4e
choelhee.lee <paekun@gmail.com>
2018-11-07 14:51:58 +0900

    ...

--- bin/gprop.sh
+++ bin/gprop.sh
@@ -15,6 +15,14 @@ select
 from 
     PERFORMANCE_VIEW_SCHEMA.v\$property
 where
-    property_name like '%$STR%' order by 1,2,3,4,5,6,7;
+    property_name like '%$STR%' 
+    order by 
+    case startup_phase when 'NONE' then 1
+                       when 'NO_MOUNT BELOW' then 2
+                       when 'NO_MOUNT ABOVE' then 3
+                       when 'MOUNT BELOW'    then 4
+                       when 'MOUNT ABOVE'    then 5
+    end,
+    1,3,4,5,6,7;
 
 EOF
